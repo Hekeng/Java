e4ecb04f74e679fd575a9891cb23d849114f6e4a
Hekeng <129593517+Hekeng@users.noreply.github.com>
2025-12-22 11:05:36 +0100

    вапвыпв

--- src/main/java/practice/TestMain.java
+++ src/main/java/practice/TestMain.java
@@ -1,8 +1,15 @@
 package practice;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class TestMain {
-	public static void main(String[] args) {
-		System.out.println( SafeDivider.safeDivide(100, -10) );
+	public static void main(String... args) {
+
+		ArrayList<String> list = new ArrayList<>(Arrays.asList("Test", "Test2", "Test3"));
+		System.out.println(list.size());
 	}
 
-}
+	}
+
+
